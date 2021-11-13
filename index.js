8dca92229b499f57a399c2d853d7c586eab5320b
Ruhul Amin <ruhuliuice@gmail.com>
2021-11-13 08:51:35 +0600

    commit

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World Heroku!");
 });
 
 app.listen(port, () => {
